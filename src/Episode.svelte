7ddbeaea7df3f75a781330185fdb86d5b03b87dd
Matt McLeod <matt@accessibilityoz.com>
2019-06-09 18:24:18 +1000

    Initial commit

---
+++ src/Episode.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let id;
+    export let date;
+    export let title;
+    export let file;
+    export let base_url = "https://boggle.sfo2.digitaloceanspaces.com/the-bugle";
+</script>
+
+<style>
+    .id {
+        grid-column: 1;
+        align-self: center;
+    }
+    .title {
+        grid-column: 2;
+        align-self: center;
+    }
+    .play {
+        grid-column: 4;
+        background: rgb(16, 159, 16);
+        text-align: center;
+        color: white;
+        padding: 4px 8px;
+        align-self: center;
+        border-radius: 6px;
+    }
+    a {
+        color: white;
+        text-decoration: none;
+    }
+    a:hover, a:focus {
+        background-color: #f1582d;
+    }
+    a:visited {
+        background-color: darkgray;
+    }
+</style>
+
+    <span class="id">{ id }</span>
+    <span class="title">{ title }</span>
+    <a class="play" aria-label="Play episode { id}: { title }" href="{base_url}/{ file }">Play</a>
---
+++ src/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<style>
+h1 {
+    background-color: #f1582d;
+    color: white;
+    margin-top: 0;
+    text-align: center;
+    padding: 4px 8px;
+}
+</style>
+
+<h1>The Bugle Podcast Archive</h1>
